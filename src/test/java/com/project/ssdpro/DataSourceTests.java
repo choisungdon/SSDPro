5f40d68223f5fd63c5e8115ed908c0829da6ea2d
DDE <DDE@221.166.167.98>
2021-07-31 18:49:49 +0900

    21-07-31

---
+++ src/test/java/com/project/ssdpro/DataSourceTests.java
@@ -0,0 +1,45 @@
+package com.project.ssdpro;
+
+import static org.junit.Assert.fail;
+
+import java.sql.Connection;
+
+import javax.sql.DataSource;
+
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import lombok.Setter;
+import lombok.extern.log4j.Log4j;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("file:src/main/webapp/WEB-INF/spring/root-context.xml")
+@Log4j
+public class DataSourceTests {
+	
+	@Setter(onMethod_ = {@Autowired})
+	private DataSource datasource;
+	
+	@Setter(onMethod_ = {@Autowired})
+	private SqlSessionFactory sqlSesstionFactory;
+	
+	@Test
+	public void testMyBatis() {
+		
+		try (SqlSession sesstion = sqlSesstionFactory.openSession();
+			 Connection con = sesstion.getConnection();)
+		{
+			log.info(sesstion);
+			log.info(con);
+		} catch (Exception e) {
+			fail(e.getMessage());
+		}
+		
+	}
+	
+}
